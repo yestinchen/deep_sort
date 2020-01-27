091b41f585c1a204bfd1cafbef5f62ef40c60ee0
ytchen <873915231@qq.com>
2020-01-26 20:34:36 -0500

    update.

---
+++ detect.sh
@@ -0,0 +1,1 @@
+python tools/single_detection_gen.py --mot_dir=/media/ytchen/hdd/working/beindex --sequence=visualroad1
---
+++ run.sh
@@ -0,0 +1,12 @@
+# python deep_sort_app.py \
+#     --sequence_dir=/media/ytchen/hdd/dataset/MOT16/test/MOT16-06 \
+#     --detection_file=./resources/detections/MOT16_POI_test/MOT16-06.npy \
+#     --min_confidence=0.3 \
+#     --nn_budget=100 \
+#     --display=True
+python deep_sort_app.py \
+    --sequence_dir=/media/ytchen/hdd/working/beindex/visualroad1 \
+    --detection_file=./detections/visualroad1.npy \
+    --min_confidence=0.3 \
+    --nn_budget=100 \
+    --display=True
